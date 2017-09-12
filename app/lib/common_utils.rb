56393504fc4815f31a2acbf5b24c5cd9bcda2378
Aniket Garg <aniketgarg@outlook.com>
2017-09-12 15:28:22 +0530

    add 10 sec offset to allow prices to be updated on website

--- app/lib/common_utils.rb
+++ app/lib/common_utils.rb
@@ -7,8 +7,9 @@ module CommonUtils
     # calculates the remaining time in which cache will expire
     # set to 6 AM everyday
     def cache_expiry
-      time = Time.zone.now.end_of_day + PRICE_REFRESH_HOUR.hours
+      time = Time.zone.now.end_of_day + PRICE_REFRESH_HOUR.hours + 10.seconds
       time -= 1.day if Time.zone.now.hour < PRICE_REFRESH_HOUR
+      byebug
       return time - Time.zone.now
     end
 
